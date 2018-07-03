1336b15135b22fe5b4266b2e82fca576674dd254
artskin <artskin@163.com>
2018-07-03 17:21:18 +0800

    add case

--- js/case.js
+++ js/case.js
@@ -85,7 +85,7 @@ function deepCopy(){
 }
 
 //case6:斐波那契
-function feibo(len) { 
+function feibo(len) {
   var fbarr = [1,1];
   for(let n =2;n<len;n++){
     fbarr[n] = fbarr[n-1]+fbarr[n-2];
@@ -93,3 +93,208 @@ function feibo(len) {
   return fbarr;
 }
 //console.log(feibo(30));
+
+
+//case7:
+// var foo =1;
+// function bar(){
+//   foo = 10;
+//   return;
+//   function foo(){}
+// }
+// bar();
+// console.log(foo);
+
+// function bar(){
+//   return foo;
+//   foo = 10;
+//   function foo(){}
+//   var foo = 11
+// }
+// console.log( typeof bar())
+
+// var x=3;
+// var foo = {
+//   x:2,
+//   baz:{
+//     x:1,
+//     bar:function(){
+//       return this.x;
+//     }
+//   }
+// }
+// var go = foo.baz.bar;
+// console.log(foo); //1
+// console.log(go); //Cannot read property 'x' of undefined
+// console.log(foo.baz.bar());//1
+
+// //case8:
+// function out(){
+  
+//   console.log("原有的out()",this);
+//   console.log(out.count);
+// }
+// var count = 1;
+// out.count = 1;
+// //out();
+// console.log( typeof out);
+
+// function out(){
+//   console.log(this.count);
+// }
+
+// var obj = {
+//   count:2,
+//   out:out
+// }
+// console.log( typeof out);
+// console.log( typeof obj.out);
+// console.log( obj.out == out);
+// obj.out();
+
+// var myObj ={
+//   out:'bar',
+//   func:function(){
+//     var self = this;
+//     console.log(this.out);
+//     console.log(self.out);
+//     (function(){
+//       console.log(this)
+//       console.log(self.out)
+//     })()
+//   }
+// };
+// myObj.func();
+
+// //case9:
+// function mm(){
+//   this.a = a;
+// }
+// mm.prototype.b =2;
+// //var aa = new mm();
+// function bar(){};
+// bar.prototype = mm.prototype
+// var b = new bar();
+// console.log(b.a)
+// console.log(b.b)
+
+// new Promise((resolve,reject) => {
+//   reject(1)
+// }).catch(() => {
+//   console.log(2)
+// }).then(() => {
+//   console.log(3);
+// },() =>{
+//   console.log(4);
+// });
+
+
+// function Foo() {
+//   getName = function () {
+//     alert (1);
+//   }; 
+//   return this;
+// }
+// Foo();
+
+// var Fpp;
+// function Fpp(){
+//   getName = function(){
+//     console.log("ligang");
+//   }
+//   return this;
+// }
+// Fpp.getName = function(){
+//   console.log("hanmeimei")
+// }
+// Fpp.prototype.getName = function(){
+//   console.log("lilei")
+// }
+// var getName = function(){
+//   console.log("www");
+// }
+// function getName(){
+//   console.log("ssss")
+// }
+// console.log(typeof Fpp)
+// console.log(Fpp())
+// console.log("Fpp.getName() =>")
+// Fpp.getName();
+// console.log("getName() =>")
+// getName();
+// // Fpp().getName();
+// console.log("new Fpp.getName() =>")
+// new Fpp.getName();
+// console.log("new Fpp().getName() =>")
+// new Fpp().getName();
+// console.log("new new Fpp().getName() =>")
+// new new Fpp().getName();
+
+// //case10:
+// function fun(n,o){
+//   console.log("o",o);
+//   return{
+//     fun:function(m){
+//       //console.log("m",m);
+//       return fun(m,n);
+//     }
+//   };
+// }
+// console.log(fun().fun())
+// fun(10)
+// console.log("a = fun(10);")
+// var a = fun(10);
+// a.fun(20);
+
+// console.log("1=====>")
+// var a = fun(1000);
+// a.fun(100);
+// a.fun(1,0);
+// a.fun(2);
+// a.fun(2,0);
+// a.fun(3);
+// a.fun(3,0);
+// console.log("<=====1")
+
+// var b = fun(0).fun(1).fun(2).fun(3);
+
+// console.log("2=====")
+// var c = fun(0).fun(1);
+// c.fun(2);
+// c.fun(3);
+// c.fun(4);
+// console.log("3=====")
+
+//case11:this
+// var o = {
+//   user:"追梦子",
+//   fn:function(){
+//       console.log(this.user); //追梦子
+//   }
+// }
+// o.fn();
+
+// var o = {
+//   a:10,
+//   b:{
+//       a:12,
+//       fn:function(){
+//         console.log(this.a); //12
+//       }
+//   }
+// }
+
+// o.b.fn();
+
+// var o = {
+//   a:10,
+//   b:{
+//       a:12,
+//       fn:function(){
+//         //console.log(this.a); //undefined
+//         console.log(this); //window
+//       }
+//   }
+// }
+// var j = o.b.fn;
+// j();
