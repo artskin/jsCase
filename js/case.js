061ab387f6eb8726a09924618821e828243f4439
amu <amu@amudeMacBook-Pro.local>
2018-08-07 11:11:31 +0800

    add

--- js/case.js
+++ js/case.js
@@ -8,6 +8,10 @@ $("#file").change(function(){
 function uploadFile(obj){
   var fileData = new FormData();
   fileData.append("file",obj[0].files[0]);
+  console.log(obj[0].files[0]);
+
+  imgPreview($('.preview img'),obj[0].files[0])
+
   $.ajax({
     url : 'http://106.14.7.135:8081/upload/image',
     type : 'POST',
@@ -15,10 +19,24 @@ function uploadFile(obj){
     processData : false, //告诉jQ不要去处理发送的数据
     contentType : false, //告诉jQ不要设置Content-Type请求头
     data : fileData,
-    success:function(data){
-      console.log(data)
+    success:function(res){
+      console.log(res.data);
+      //.attr('src','http://106.14.7.135:8081/'+res.data.key)
+      
     }
   })
+}
+function imgPreview(dom,fileName){
+  if(fileName){
+    var reader = new FileReader();
+    reader.readAsDataURL(fileName);
+    reader.onload = function(event){
+      var e = event || window.event;
+      //dom.src = this.result;
+      console.log(this.result);
+      dom.attr('src',this.result)
+    }
+  }
 }
 
 
