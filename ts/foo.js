55f6c44e7463b070aa2879d31d24b8ce6ce5bf3b
Guliqiang <guliqiang@sensetime.com>
2020-02-26 19:44:39 +0800

    vscode debug typescript

--- ts/foo.js
+++ ts/foo.js
@@ -1,5 +1,23 @@
 function greeter(person) {
     return "hello" + person.firstName + "-" + person.lastName;
 }
-var user = { firstName: "Gu", lastName: 'amu' };
+let user = { firstName: "Gu", lastName: 'amu' };
 document.body.innerHTML = greeter(user);
+let dogs = ['阿黄'];
+console.log(dogs);
+let Num = [1, 2, 3];
+console.log(Num);
+let objs = [{ a: 1 }, { a: 2 }];
+console.log(objs);
+class Observable {
+}
+Array.prototype.toObservable = function () {
+    return new Observable();
+};
+let colors = ['#fff', '#000'];
+let list = [2, 3, 4];
+console.log(colors);
+console.log(list);
+console.log('测试3');
+//配置好tsconfig.json在根目录运行tsc，编译所有文件
+//tsc --watch/w 实时监控ts变化并编译
--- ts/foo.ts
+++ ts/foo.ts
@@ -10,3 +10,37 @@ function greeter(person :Person){
 let user = {firstName:"Gu",lastName:'amu'};
 
 document.body.innerHTML = greeter(user)
+
+interface Items<T> {
+  [index:number]:T
+}
+
+let dogs:Items<string> = ['阿黄']
+console.log(dogs)
+
+let Num:Items<number> = [1,2,3]
+console.log(Num)
+
+let objs:Items<object> = [{a:1},{a:2}]
+console.log(objs)
+
+class Observable<T> {
+  // ... implementation left as an exercise for the reader ...
+}
+
+interface Array<T> {
+  toObservable(): Observable<T>;
+}
+
+Array.prototype.toObservable = function () {
+  return new Observable();
+}
+
+let colors: Array<string> = ['#fff','#000']
+let list: Array<number> = [2,3,4]
+console.log(colors)
+console.log(list)
+console.log('测试3')
+
+//配置好tsconfig.json在根目录运行tsc，编译所有文件
+//tsc --watch/w 实时监控ts变化并编译
