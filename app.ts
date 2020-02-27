a7b568070f6660d57d41f1c3ee1411c35a98539c
Guliqiang <guliqiang@sensetime.com>
2020-02-27 19:29:57 +0800

    vscode debug typescript

--- app.ts
+++ app.ts
@@ -1,1 +1,26 @@
-console.log("hello TypeScript"); 
+console.log("hello TypeScript");
+
+class Animal {
+    name:string
+    meters:number
+
+    constructor(){
+        this.name = name
+    }
+    move(){
+        console.log(this.name,'每秒钟移动',this.meters,'米')
+    }
+}
+
+class Dog extends Animal {
+    move(){
+        console.log(this.name,'每秒钟跳跃',this.meters,'米')
+    }
+}
+
+const ahuang = new Dog()
+console.log(ahuang)
+
+
+let colors:Array<string> =[];
+console.log(colors);
--- ts/foo.ts
+++ ts/foo.ts
@@ -3,20 +3,16 @@ interface Person{
   lastName:string;
 }
 
-function greeter(person :Person){
+export function greeter(person :Person){
   return "hello"+ person.firstName +"-"+person.lastName;
 }
-
-let user = {firstName:"Gu",lastName:'amu'};
-
-document.body.innerHTML = greeter(user)
 
 interface Items<T> {
   [index:number]:T
 }
-
+console.log(1212)
 let dogs:Items<string> = ['阿黄']
-console.log(dogs)
+console.log(typeof dogs,dogs)
 
 let Num:Items<number> = [1,2,3]
 console.log(Num)
@@ -32,15 +28,15 @@ interface Array<T> {
   toObservable(): Observable<T>;
 }
 
-Array.prototype.toObservable = function () {
-  return new Observable();
-}
+// Array.prototype.toObservable = function () {
+//   return new Observable();
+// }
 
-let colors: Array<string> = ['#fff','#000']
-let list: Array<number> = [2,3,4]
-console.log(colors)
-console.log(list)
-console.log('测试3')
+// let colors: Array<string> = ['#fff','#000']
+// let list: Array<number> = [2,3,4]
+// console.log(colors)
+// console.log(list)
+// console.log('测试3')
 
 //配置好tsconfig.json在根目录运行tsc，编译所有文件
 //tsc --watch/w 实时监控ts变化并编译
